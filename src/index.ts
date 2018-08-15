ca1ca7d79a356a1ae2f408cc714e5e014d4f1046
small_sunshine <jioo0224@naver.com>
2018-08-15 16:35:02 +0900

    TypeScript로 수정

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import * as Discord from 'discord.js'
+const client = new Discord.Client()
+const config = require('../config.json')
+import * as log4js from 'log4js'
+
+const logger = log4js.getLogger()
+
+try {
+  if (process.env.NODE_ENV === 'production') {
+    logger.level = 'INFO'
+  } else if (process.env.NODE_ENV === 'development') {
+    logger.level = 'DEBUG'
+  } else {
+    logger.level = 'DEBUG'
+    process.env.NODE_ENV = 'development'
+  }
+  logger.info(`${process.env.NODE_ENV} mode`)
+
+  client.on('ready', () => {
+    logger.info(`Logged in as ${client.user.tag}!`)
+    logger.debug('Debug Mode!')
+  });
+
+  client.on('message', msg => {
+    if (msg.content === 'ping') {
+      logger.info('ping')
+      msg.reply('pong')
+    }
+  });
+
+  client.login(config.token)
+} catch (e) {
+  console.log(e)
+}
